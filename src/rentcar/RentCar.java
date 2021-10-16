c57007fd7d50e55f2721e236f8041c787c5aa0cb
bimapw4 <bimapw4@gmail.com>
2021-10-16 10:50:53 +0700

    + initial commit + add page login register

---
+++ src/rentcar/RentCar.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rentcar;
+
+/**
+ *
+ * @author Admin
+ */
+public class RentCar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        Pemesanan_page pemesanan = new Pemesanan_page();
+//        pemesanan.setVisible(true);
+    }
+    
+}
